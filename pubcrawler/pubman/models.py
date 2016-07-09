cb95f5cd7d3aa5df156f425b94844a01b650b178
Jônatas Rech <jonatas.rech@gmail.com>
2016-07-09 19:39:52 -0300

    Adding the first models: Author and Publication

--- pubcrawler/pubman/models.py
+++ pubcrawler/pubman/models.py
@@ -1,5 +1,25 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length = 100)
+    last_name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+    class Meta:
+        ordering = ('last_name',)
+
+class Publication(models.Model):
+    authors = models.ManyToManyField(Author)
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    date_added = models.DateTimeField('date added')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('date_added',)
